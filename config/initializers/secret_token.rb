6ce25428f74bd978155ff95fc731d0823248fd1c
Carlos Peralta <caperaltasanchez@gmail.com>
2012-03-24 13:46:13 -0500

    Ejemplo con Rufus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppRufus::Application.config.secret_token = '084edd25dac61b1bb7856bc34c4ca5a09fce98d574429b3c8c35dc7bb669418e0241f3ac0da689761649f8ab265541570f3a44d1efef5a1b46b19c8d17a50489'
